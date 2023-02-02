f5e8bebc374c3d894dc7af80e329a8317ee8d591
Pavel Spichakov <spv-pavel@yandex.ru>
2023-02-02 22:55:59 +0300

    Main: cleaning

--- main.py
+++ main.py
@@ -4,27 +4,21 @@ from myclass import Board, Ship, Dot, User, AI, Game
 player_field = [['O' for x in range(6)] for y in range(6)]
 computer_field = [['O' for x1 in range(6)] for y1 in range(6)]
 
-p_dot1, p_dot2, p_dot3 = Dot(1 - 1, 1 - 1), Dot(3 - 1, 3 - 1), Dot(1 - 1, 6 - 1)
-p_ship1, p_ship2, p_ship3 = Ship(p_dot1, 3, 'v'), Ship(p_dot2, 2), Ship(p_dot3, 3, 'v')
-player_ships = [p_ship1, p_ship2, p_ship3]
-player_living_ships = player_ships
-p_dots1, p_dots2, p_dots3 = p_ship1.dots, p_ship2.dots, p_ship3.dots
-
 # start_length_ships = [3, 2, 2, 1, 1, 1, 1]
 start_length_ships = [3, 2]
-# player_ships, player_living_ships = [], []
+player_ships, player_living_ships = [], []
 computer_ships, computer_living_ships = [], []
 player_board = Board('player', player_field, player_ships, player_living_ships, True)
 computer_board = Board('computer', computer_field, computer_ships, computer_living_ships, True)
 player = User(player_board, computer_board)
 computer = AI(computer_board, player_board)
 
-play = Game(player, player_board, computer, computer_board)
+game = Game(player, player_board, computer, computer_board)
 
-play.greet(start_length_ships)
-play.random_board(start_length_ships)
+game.greet(start_length_ships)
+game.random_board(start_length_ships)
 player_board.print_board()
-# computer_board.print_board()
+computer_board.print_board()
 
 while True:
     while True:
